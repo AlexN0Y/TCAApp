e8fa76d9da3441d50d81aa57681d9f8f4cb9afd2
AlexN0Y <gao.sanya@gmail.com>
2025-04-21 20:53:52 +0300

    Initial Commit

---
+++ SkillsEnhancement/SkillsEnhancementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillsEnhancementApp.swift
+//  SkillsEnhancement
+//
+//  Created by Alex Gav on 21.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SkillsEnhancementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
